b42e633c72cb8dc05cdc6b9baec139e299fe6969
BitBelle <wainainamercy432@gmail.com>
2024-10-04 16:21:04 +0300

    Explored functions and their side-effects

---
+++ functions/functions+Sideeffects/README.md
@@ -0,0 +1,14 @@
+**Pure Functions**
+
+This are operations that produce the same output for the same input. Meaning they return the same results when you pass the same arguements.
+
+Pure functions are:
+    Readable - 
+
+    Reusable -
+
+    Predictable -
+
+    Testable - 
+
+
---
+++ functions/functions+Sideeffects/test.js
@@ -0,0 +1,22 @@
+// function returning a value explicitly
+
+// func definition
+function personalDetails(name, age){
+
+    // returning the string value
+    return `My name is ${name} and I'm ${age}yrs old`
+
+}
+
+// invoking the function
+personalDetails("mercy", 20);
+
+
+
+// function not returning a value explicitly
+function testMe(){
+
+    // doesnt return anything
+}
+
+testMe(); //returns undefined
